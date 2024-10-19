9044fdfcbeed50000733a48ca1615c75d1c3ec45
Priyavrat Kumar <122096583+Priyavrat001@users.noreply.github.com>
2024-10-19 23:40:49 +0530

    Allowing cors origin

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}))
+app.use(cors({}))
 app.use(cookieParser());
 connectToDB();
 
